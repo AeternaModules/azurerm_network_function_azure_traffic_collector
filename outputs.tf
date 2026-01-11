9fefd801b11e6dd4a9cec17746f5ffcb13ba6ce1
Daniel Allen <dan@aeternagroup.net>
2026-01-11 16:51:40 +0000

    Update to schema version v4.57.0

---
+++ outputs.tf
@@ -0,0 +1,5 @@
+output "network_function_azure_traffic_collectors" {
+  description = "All network_function_azure_traffic_collector resources"
+  value       = azurerm_network_function_azure_traffic_collector.network_function_azure_traffic_collectors
+}
+
