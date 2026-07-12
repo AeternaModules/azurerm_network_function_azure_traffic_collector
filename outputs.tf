ca15e25483f7117841784ef57c84bc6255b1f5cb
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:19:15 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,29 +1,25 @@
-output "network_function_azure_traffic_collectors" {
-  description = "All network_function_azure_traffic_collector resources"
-  value       = azurerm_network_function_azure_traffic_collector.network_function_azure_traffic_collectors
-}
 output "network_function_azure_traffic_collectors_collector_policy_ids" {
-  description = "List of collector_policy_ids values across all network_function_azure_traffic_collectors"
-  value       = [for k, v in azurerm_network_function_azure_traffic_collector.network_function_azure_traffic_collectors : v.collector_policy_ids]
+  description = "Map of collector_policy_ids values across all network_function_azure_traffic_collectors, keyed the same as var.network_function_azure_traffic_collectors"
+  value       = { for k, v in azurerm_network_function_azure_traffic_collector.network_function_azure_traffic_collectors : k => v.collector_policy_ids }
 }
 output "network_function_azure_traffic_collectors_location" {
-  description = "List of location values across all network_function_azure_traffic_collectors"
-  value       = [for k, v in azurerm_network_function_azure_traffic_collector.network_function_azure_traffic_collectors : v.location]
+  description = "Map of location values across all network_function_azure_traffic_collectors, keyed the same as var.network_function_azure_traffic_collectors"
+  value       = { for k, v in azurerm_network_function_azure_traffic_collector.network_function_azure_traffic_collectors : k => v.location }
 }
 output "network_function_azure_traffic_collectors_name" {
-  description = "List of name values across all network_function_azure_traffic_collectors"
-  value       = [for k, v in azurerm_network_function_azure_traffic_collector.network_function_azure_traffic_collectors : v.name]
+  description = "Map of name values across all network_function_azure_traffic_collectors, keyed the same as var.network_function_azure_traffic_collectors"
+  value       = { for k, v in azurerm_network_function_azure_traffic_collector.network_function_azure_traffic_collectors : k => v.name }
 }
 output "network_function_azure_traffic_collectors_resource_group_name" {
-  description = "List of resource_group_name values across all network_function_azure_traffic_collectors"
-  value       = [for k, v in azurerm_network_function_azure_traffic_collector.network_function_azure_traffic_collectors : v.resource_group_name]
+  description = "Map of resource_group_name values across all network_function_azure_traffic_collectors, keyed the same as var.network_function_azure_traffic_collectors"
+  value       = { for k, v in azurerm_network_function_azure_traffic_collector.network_function_azure_traffic_collectors : k => v.resource_group_name }
 }
 output "network_function_azure_traffic_collectors_tags" {
-  description = "List of tags values across all network_function_azure_traffic_collectors"
-  value       = [for k, v in azurerm_network_function_azure_traffic_collector.network_function_azure_traffic_collectors : v.tags]
+  description = "Map of tags values across all network_function_azure_traffic_collectors, keyed the same as var.network_function_azure_traffic_collectors"
+  value       = { for k, v in azurerm_network_function_azure_traffic_collector.network_function_azure_traffic_collectors : k => v.tags }
 }
 output "network_function_azure_traffic_collectors_virtual_hub_id" {
-  description = "List of virtual_hub_id values across all network_function_azure_traffic_collectors"
-  value       = [for k, v in azurerm_network_function_azure_traffic_collector.network_function_azure_traffic_collectors : v.virtual_hub_id]
+  description = "Map of virtual_hub_id values across all network_function_azure_traffic_collectors, keyed the same as var.network_function_azure_traffic_collectors"
+  value       = { for k, v in azurerm_network_function_azure_traffic_collector.network_function_azure_traffic_collectors : k => v.virtual_hub_id }
 }
 
